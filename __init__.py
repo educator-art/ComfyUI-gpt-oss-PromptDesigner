eb669adfa5fe7a3bf40d13cae35391cdd8445c84
e <153011054+educator-art@users.noreply.github.com>
2025-08-11 23:45:18 +0900

    1st commit

---
+++ __init__.py
@@ -0,0 +1,8 @@
+from .nodes.nodes import *
+from .nodes.nodes_advanced import *
+
+NODE_CLASS_MAPPINGS = {
+    "Load gpt-oss Prompt Designer": LoadGPTOSSPromptDesigner,
+}
+
+print("Load Class: LoadGPTOSSPromptDesigner")
---
+++ nodes/nodes.py
@@ -0,0 +1,89 @@
+"""
+usage system:
+- Google Colab L4 GPU 
+    - option: High Memory
+
+note:
+Google Colab has the OpenAI library installed.
+openai 1.99.1 (Checked: 2025.08.11)
+"""
+from openai import OpenAI
+
+system_prompt="""
+You are Stable Diffusion Prompt Designer.
+"""
+
+user_prompt="""
+# 依頼
+以下の条件に従ってプロンプトを英語で作成してください
+
+## プロンプトのテーマ
+美しい風景
+
+## 単語数
+30単語程度
+
+## Output
+prompt: 
+"""
+
+class LoadGPTOSSPromptDesigner:
+
+    def __init__(self):
+
+        self.system_prompt=""
+        self.user_prompt=""
+
+    @classmethod
+    def INPUT_TYPES(cls):
+        return {
+            "required": {
+                "system": (
+                    "STRING",
+                    {"multiline": True, "default": system_prompt},
+                ),
+            },
+            "required": {
+                "user": (
+                    "STRING",
+                    {"multiline": True, "default": user_prompt},
+                ),
+            }
+        }
+
+    # After Inference, Default output: "Stable Diffusion Prompt"
+    RETURN_TYPES = ("STRING",)
+    FUNCTION = "inference"
+    OUTPUT_NODE = True
+    CATEGORY = "Load Images and Prompts from Directory"
+
+    def inference(self, system, user):
+
+        # if text is empty, display error
+        if not system:
+            raise ValueError(f"system promptが空です")
+        
+        if not user:
+            raise ValueError(f"user promptが空です")
+        
+        self.system_prompt=system_prompt
+        self.user_prompt=user_prompt
+
+
+        client = OpenAI(
+            base_url="http://localhost:11434/v1", # Local Ollama API
+            api_key="ollama" # Dummy key
+        )
+
+        response = client.chat.completions.create(
+            model="gpt-oss:20b", # model: gpt-oss:20b
+            messages=[
+                {"role": "system", "content": self.system_prompt},
+                {"role": "user", "content": self.user_prompt}
+            ]
+        )
+
+        # return prompt 
+        print(response.choices[0].message.content)
+        return (response.choices[0].message.content,)
+
